8aaa1e1ec96f6e4797b5af52aef0ef2c430a88b2
moidev <moisesferia@MacBook-Air-de-Get.local>
2024-12-13 00:48:00 -0600

    Actualizacion día 5

--- dia_5/Funciones Dinamicas.py
+++ dia_5/Funciones Dinamicas.py
@@ -6,3 +6,11 @@ def revision(numero):
 si es que cuenta con 3 cifras o no"""
 resultado =  revision(10)
 print(resultado)
+
+
+def revision_3_cifras(numero):
+    return numero in range(100,1000)
+
+suma = 586 + 402
+resultado = revision_3_cifras(suma)
+print(resultado)
